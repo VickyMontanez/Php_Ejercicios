047eec577b6f49050dfd4186f0f9c0476e6711c2
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-05-27 08:35:26 -0500

    feat: :sparkles: Add a First exercise of Php
    Add a first exercise about write a function that returns the string "Hello, World!".

--- api.php
+++ api.php
@@ -1,5 +1,13 @@
 <?php
 
 //EJERCICIOS DE PHP PARA PRACTICAR
+    
+/* 1. Write a function that returns the string "Hello, World!" */
+
+    function helloWorld(){
+        return "Hello, World!";
+    };
+
+    echo helloWorld();
 
 ?>
