3d0ae97c58b94f578f3c3702266e4f960edbe5f2
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-05-28 19:45:47 -0500

    feat: :sparkles: Add the Fourth exercise of Php
    Add the fourth exercise about calculate the Hamming difference between two DNA strands

--- api.php
+++ api.php
@@ -2,12 +2,21 @@
 
 //EJERCICIOS DE PHP PARA PRACTICAR
     
-/* 3. Convert a resistor band's color to its numeric representation*/
-const COLORS = array("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white");
-
-function colorCode(string $color): int{
-    return array_search($color, COLORS);
+/* 4. Convert a resistor band's color to its numeric representation*/
+function distance(string $str1, string $str2){
+    $length1 = strlen($str1);
+    $length2 = strlen($str2);
+    $diferences = 0;
+    if ($length1 === $length2){
+        $arr1 = str_split($str1);
+        $arr2 = str_split($str2);
+            for ($i = 0; $i < $length1; $i++){
+                if ($arr1[$i] !== $arr2[$i]) {
+                    $diferences++;
+                };
+            }
+        return $diferences;
+    }else{
+        throw new InvalidArgumentException('DNA strands must be of equal length');
+    };
 };
-
-echo colorCode("orange");
-?>
