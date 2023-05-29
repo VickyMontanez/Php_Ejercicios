eb3c41d49b117c93eae5a665128b1391019d2aa1
VickyMontanezCampus <vmontanez707@gmail.com>
2023-05-29 07:47:04 -0500

    feat: :sparkles: Add rhe Fifth exercise of Php
    Add the fifth exercise about determine the moment that would be after a gigasecond has passed.

--- api.php
+++ api.php
@@ -2,21 +2,11 @@
 
 //EJERCICIOS DE PHP PARA PRACTICAR
     
-/* 4. Convert a resistor band's color to its numeric representation*/
-function distance(string $str1, string $str2){
-    $length1 = strlen($str1);
-    $length2 = strlen($str2);
-    $diferences = 0;
-    if ($length1 === $length2){
-        $arr1 = str_split($str1);
-        $arr2 = str_split($str2);
-            for ($i = 0; $i < $length1; $i++){
-                if ($arr1[$i] !== $arr2[$i]) {
-                    $diferences++;
-                };
-            }
-        return $diferences;
-    }else{
-        throw new InvalidArgumentException('DNA strands must be of equal length');
-    };
-};
+/* 5. Given a moment, determine the moment that would be after a gigasecond has passed.
+*/
+declare(strict_types=1);
+
+function from(DateTimeImmutable  $date):DateTimeImmutable{
+    return $date->modify("+1000000000 seconds");
+}
+?>
